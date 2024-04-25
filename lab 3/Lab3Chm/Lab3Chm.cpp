ef76b2d57eb037f1e6dcbe71ecc1dbe709ca71a0
zamaraevnikita <nikzam241104@gmail.com>
2024-04-25 20:58:23 +0700

    first

---
+++ lab 3/Lab3Chm/Lab3Chm.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "Eigenvalue.h"
+using namespace luMath;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    Eigenvalue<double> matrix;
+
+    switch (matrix.getTask())
+    {
+    case Eigenvalue<double>::TASK::EIGENVALUES:
+        matrix.getEigenvalues();
+        break;
+    case Eigenvalue<double>::TASK::EIGENVECTORS:
+        matrix.getEigenvalues();
+        matrix.getEigenvectors();
+        break;
+    case Eigenvalue<double>::TASK::ROOTS:
+        matrix.GetRoots();
+        break;
+    }
+
+    return 0;
+}
+
